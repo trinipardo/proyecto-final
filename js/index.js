3d18f4b083c51e80c164d0ed224e80c0df3fba62
lucia <lsalasgonzalez@udesa.edu.ar>
2019-11-09 16:50:30 -0300

    terminando css y empezando javascript

---
+++ js/index.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+
+  var homeLink = document.getElementById('homeLink');
+  var peliculasLink = document.getElementById('peliculasLink');
+  var seriesLink = document.getElementById('seriesLink');
+  var seriesGenero = document.getElementsByClassName('seriesGenero')
+  homeLink.onclick = function(){
+    window.location.href = 'index.html';
+  }
+
+  peliculasLink.onclick = function(){
+      window.location.href = 'pag2.html';
+
+
+  }
+seriesLink.onclick = function (){
+  window.location.href = 'pag3.html';
+}
+ seriesGenero.onclick = function (){
+   window.location.href = 'pag4.html'
+ }
+
+
+
+
+}
