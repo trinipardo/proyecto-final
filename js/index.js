f6b80e81861a52b6a16bfeafafc0070bf226b920
Trinidad Pardo <trinipardo@hotmail.com>
2019-11-09 17:04:14 -0300

    api key

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var apiKey = "e3bcfbf11e6b8143b068f8b59c89e7bf";
+var apiReadKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2JjZmJmMTFlNmI4MTQzYjA2OGY4YjU5Yzg5ZTdiZiIsInN1YiI6IjVkYzcwNDkxOWMyNGZjMDAxNWMyOTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VJOtHTzGUTC_LsCop_LYgvqj9UlM9SsKUxCwIaqRkaI";
+var apiURL = "https://api.themoviedb.org/3/movie/550?api_key=e3bcfbf11e6b8143b068f8b59c89e7bf";
+
 window.onload = function(){
 
   var homeLink = document.getElementById('homeLink');
