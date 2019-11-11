2488445ebcd1678a368ab424f02e528007af8774
lucia <lsalasgonzalez@udesa.edu.ar>
2019-11-11 02:39:33 -0300

    terminando el css y html de pag3 y index

--- js/index.js
+++ js/index.js
@@ -2,23 +2,23 @@ window.onload = function(){
 
   var homeLink = document.getElementById('homeLink');
   var peliculasLink = document.getElementById('peliculasLink');
-  var seriesLink = document.getElementById('seriesLink');
-  var seriesGenero = document.getElementById('seriesGenero')
+  var seriesPorGenero = document.getElementById('seriesLink');
+  //var seriesGenero = document.getElementById('seriesGenero')
   homeLink.onclick = function(){
     window.location.href = 'index.html';
   }
 
   peliculasLink.onclick = function(){
-      window.location.href = 'pag2.html';
+      window.location.href = "pag2.html";
 
 
   }
-seriesLink.onclick = function (){
-  window.location.href = 'pag3.html';
+seriesPorGenero.onclick = function (){
+  window.location.href = "pag3.html";
 }
- seriesGenero.onclick = function (){
-   window.location.href = 'pag4.html'
- }
+ //seriesGenero.onclick = function (){
+//   window.location.href = "pag4.html"
+ }//
 
 
 
