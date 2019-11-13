3d9292d67941adf0f9f5d91c98fa83e714e9057b
lucia <lsalasgonzalez@udesa.edu.ar>
2019-11-13 13:04:49 -0300

    .js de la pag 2

--- js/pag2api.js
+++ js/pag2api.js
@@ -1,3 +1,19 @@
-var apiKey = "e3bcfbf11e6b8143b068f8b59c89e7bf";
 
-var apiURL = "https://api.themoviedb.org/3/movie/550?api_key=e3bcfbf11e6b8143b068f8b59c89e7bf"
+//LISTADO DE GÉNEROS DE TMDB
+var apiKey = "e3bcfbf11e6b8143b068f8b59c89e7bf";
+var apiURL = 'https://api.themoviedb.org/3/discover/movie?api_key=8f2a16d4f0d2593718febacdf7b1d495&language=es-AR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1;
+var mostPopularmovies = /discover/movie?sort_by=popularity.desc
+
+window.onload = function(){
+
+fetch (apiURL)
+.then(function(response){ return respuesta.json(); })
+
+.then(function(datos){
+var etiquetaQueQuieroCambiar = document.getElementById('dataContainer');
+console.log(datos)
+
+
+
+
+ }
