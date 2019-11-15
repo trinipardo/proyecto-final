b53042bbfe0439a77c3b9cc03d3fb1fecd33791f
Trinidad Pardo <trinipardo@hotmail.com>
2019-11-15 09:24:43 -0300

    pag2 api

--- js/pag2.js
+++ js/pag2.js
@@ -1,4 +1,4 @@
-window.onload=function () {
+
 
 
   // if (sessionStorage.getItem("name")) {
@@ -8,27 +8,3 @@ window.onload=function () {
   // }
 
 // FETH DE PELÍCULAS POR GÉNERO
- fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1")
-  .then(function(respuesta) {
-        return respuesta.json()
-      })
-  .then(function(informacion) {
-  console.log(informacion);
-
-  var generos= informacion.genres
-  console.log(generos);
-
-// UN FOR PARA CADA GÉNERO
-  for (var i = 0; i < generos.length; i++) {
-    var id = generos[i].id
-    var nombre = generos[i].name
-
-    document.querySelector("#generos").innerHTML += "<a class=uk-text-center href=../3.PeliculasPorGenero/pagina3.html?idDeGenero=" + id + "&nombreDeGenero="+nombre+">" + nombre + "</a>"
-  }
-})
-
-  .catch(function(error) {
-   console.log("Error: " + error);
-      })
-
-}
--- js/pag2api.js
+++ js/pag2api.js
@@ -1,7 +1,7 @@
 
 //LISTADO DE GÉNEROS DE TMDB
-var apiKey = "e3bcfbf11e6b8143b068f8b59c89e7bf";
-var apiURL = 'https://api.themoviedb.org/3/discover/movie?api_key=8f2a16d4f0d2593718febacdf7b1d495&language=es-AR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1;
+// var apiKey = "e3bcfbf11e6b8143b068f8b59c89e7bf";
+/*var apiURL = 'https://api.themoviedb.org/3/genre/tv/list?language=en-US&api_key=%3C%3Capi_key%3E%3E'
 var mostPopularmovies = /discover/movie?sort_by=popularity.desc
 
 window.onload = function(){
@@ -16,4 +16,32 @@ console.log(datos)
 
 
 
- }
+}*/
+window.onload=function () {
+
+var apiURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=d6ff71a6bd2a94eaac5b986a9112d505&page=1"
+// FETH DE PELÍCULAS POR GÉNERO
+ fetch(apiURL)
+  .then(function(respuesta) {
+        return respuesta.json()
+      })
+  .then(function(informacion) {
+  console.log(informacion);
+
+  var generos= informacion.genres
+  console.log(generos);
+
+// UN FOR PARA CADA GÉNERO
+  for (var i = 0; i < generos.length; i++) {
+    var id = generos[i].id
+    var nombre = generos[i].name
+
+    document.querySelector("#generos").innerHTML += "<a class=uk-text-center href=pag3.html?idDeGenero=" + id + "&nombreDeGenero="+nombre+">" + nombre + "</a>"
+  }
+})
+
+  .catch(function(error) {
+   console.log("Error: " + error);
+      })
+
+}
